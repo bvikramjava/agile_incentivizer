0a83703af744196b823d68fdfe185de5d64a215f
Ryan McDevitt <rmcdevitt@foxgrp.net>
2014-03-28 16:07:37 -0400

    removed debug li from edit task template

--- app/services/services.js
+++ app/services/services.js
@@ -1,24 +1,26 @@
+var firebaseUrl = "https://luminous-fire-9343.firebaseio.com";
+
 app.factory("taskService", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://luminous-fire-9343.firebaseio.com/tasks");
+    var ref = new Firebase(firebaseUrl + "/tasks");
     return $firebase(ref);
 }]);
 
 app.factory("categoryService", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://luminous-fire-9343.firebaseio.com/categories");
+    var ref = new Firebase(firebaseUrl + "/categories");
     return $firebase(ref);
 }]);
 
 app.factory("instanceService", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://luminous-fire-9343.firebaseio.com");
+    var ref = new Firebase(firebaseUrl + "");
     return $firebase(ref);
 }]);
 
 app.factory("rewardService", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://luminous-fire-9343.firebaseio.com/rewards");
+    var ref = new Firebase(firebaseUrl + "/rewards");
     return $firebase(ref);
 }]);
 
 app.factory("accountService", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://luminous-fire-9343.firebaseio.com/account");
+    var ref = new Firebase(firebaseUrl + "/account");
     return $firebase(ref);
 }]);
